577338a44bdfc774c52e45bc193a12f09a176879
MUAHMMEDJASEEM <94896613+MUAHMMEDJASEEM@users.noreply.github.com>
2023-01-23 12:08:04 +0530

    Update messcut.js

--- Node/routes/messcut.js
+++ Node/routes/messcut.js
@@ -5,7 +5,7 @@ mongodb://localhost:27017
 /* GET users listing. */
 
 var mongoose = require('./db');
-const User = mongoose.model('messcuts', { name: String, usercode: Number, messcut: Boolean,time:String });
+const User = mongoose.model('messcuts', { name: String, usercode: Number, messcut: Boolean, time: String });
 router.get('/full', function (req, res, next) {
     User.find({}, (err, users) => {
         if (err) console.log("error");;
@@ -16,10 +16,10 @@ router.get('/:id', function (req, res, next) {
     User.find({ usercode: req.params.id }, (err, users) => {
         console.log(req.params.id)
         if (err) console.log("error");;
-        if (users.length %2== 1)
-            res.json({messcut:true});
+        if (users.length >= 1)
+            res.json({ messcut: true });
         else
-        res.json({messcut:false});
+            res.json({ messcut: false });
     });
 });
 router.post('/', function (req, res, next) {
