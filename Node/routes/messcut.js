99628734e533dc0292388c10a0fe9aa4a81e38f8
MUAHMMEDJASEEM <94896613+MUAHMMEDJASEEM@users.noreply.github.com>
2023-01-23 12:21:57 +0530

    Update messcut.js

--- Node/routes/messcut.js
+++ Node/routes/messcut.js
@@ -22,33 +22,49 @@ router.get('/:id', function (req, res, next) {
             res.json({ messcut: false });
     });
 });
+Copy code
 router.post('/', function (req, res, next) {
-    if (req.body.messcut) {
-        const offset = new Date().getTimezoneOffset();
-        const indianTime = new Date(new Date().getTime() + offset * 60 * 1000 + 330 * 60 * 1000);
-        const indianTimeFormat = new Intl.DateTimeFormat('en-IN', {
-            year: 'numeric', 
-            month: 'short', 
-            day: '2-digit', 
-            hour: '2-digit', 
-            minute: '2-digit', 
-            second: '2-digit' 
-        }).format(indianTime);
-        const newMessCut = new User({
-            name: req.body.name,
-            usercode: req.body.usercode,
-            messcut: req.body.messcut,
-            time: indianTimeFormat
-        });
-        newMessCut.save((err,messcut) => {
-            if (err) return console.error(err);
-            res.json(messcut);
-        });
-    } else {
-        User.deleteOne({ usercode: req.body.usercode }, (err) => {
-            if (err) return console.error(err);
-            res.json({ message: "Successfully deleted the messcut with usercode: " + req.body.usercode });
-        });
-    }
+    User.findOne({ usercode: req.body.usercode }, function(err, existingUser) {
+        if (err) return console.error(err);
+        if (req.body.messcut) {
+            if (existingUser) {
+                // usercode already exists, update existing user
+                User.updateOne({ usercode: req.body.usercode }, { messcut: req.body.messcut }, function(err, result) {
+                    if (err) return console.error(err);
+                    res.json(result);
+                });
+            } else {
+                const offset = new Date().getTimezoneOffset();
+                const indianTime = new Date(new Date().getTime() + offset * 60 * 1000 + 330 * 60 * 1000);
+                const indianTimeFormat = new Intl.DateTimeFormat('en-IN', {
+                    year: 'numeric', 
+                    month: 'short', 
+                    day: '2-digit', 
+                    hour: '2-digit', 
+                    minute: '2-digit', 
+                    second: '2-digit' 
+                }).format(indianTime);
+                const newMessCut = new User({
+                    name: req.body.name,
+                    usercode: req.body.usercode,
+                    messcut: req.body.messcut,
+                    time: indianTimeFormat
+                });
+                newMessCut.save((err,messcut) => {
+                    if (err) return console.error(err);
+                    res.json(messcut);
+                });
+            }
+        } else {
+            if (existingUser) {
+                User.deleteMany({ usercode: req.body.usercode }, (err) => {
+                    if (err) return console.error(err);
+                    res.json({ message: "Successfully deleted the messcut with usercode: " + req.body.usercode });
+                });
+            } else {
+                res.json({ message: "No user found with usercode: " + req.body.usercode });
+            }
+        }
+    });
 });
 module.exports = router;
